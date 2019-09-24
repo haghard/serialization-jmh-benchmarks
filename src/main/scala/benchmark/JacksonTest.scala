bf4f9900365fea6fae6b6ad026fe48789fd26012
haghard <haghard84@gmail.com>
2019-09-24 11:30:44 +0300

    scalafmt

--- src/main/scala/benchmark/JacksonTest.scala
+++ src/main/scala/benchmark/JacksonTest.scala
@@ -36,37 +36,37 @@ object JacksonTest {
 
   def readItem(map: Map[String, Any]): Data.Type =
     Data.Type(
-      `_id` = map.get("_id").get.asInstanceOf[String],
-      `index` = map.get("index").get.asInstanceOf[Int],
-      `guid` = map.get("guid").get.asInstanceOf[String],
-      `isActive` = map.get("isActive").get.asInstanceOf[Boolean],
-      `balance` = map.get("balance").get.asInstanceOf[String],
-      `picture` = map.get("picture").get.asInstanceOf[String],
-      `age` = map.get("age").get.asInstanceOf[Int],
-      `eyeColor` = map.get("eyeColor").get.asInstanceOf[String],
-      `name` = map.get("name").get.asInstanceOf[String],
-      `gender` = map.get("gender").get.asInstanceOf[String],
-      `company` = map.get("company").get.asInstanceOf[String],
-      `email` = map.get("email").get.asInstanceOf[String],
-      `phone` = map.get("phone").get.asInstanceOf[String],
-      `address` = map.get("address").get.asInstanceOf[String],
-      `about` = map.get("about").get.asInstanceOf[String],
+      `_id`        = map.get("_id").get.asInstanceOf[String],
+      `index`      = map.get("index").get.asInstanceOf[Int],
+      `guid`       = map.get("guid").get.asInstanceOf[String],
+      `isActive`   = map.get("isActive").get.asInstanceOf[Boolean],
+      `balance`    = map.get("balance").get.asInstanceOf[String],
+      `picture`    = map.get("picture").get.asInstanceOf[String],
+      `age`        = map.get("age").get.asInstanceOf[Int],
+      `eyeColor`   = map.get("eyeColor").get.asInstanceOf[String],
+      `name`       = map.get("name").get.asInstanceOf[String],
+      `gender`     = map.get("gender").get.asInstanceOf[String],
+      `company`    = map.get("company").get.asInstanceOf[String],
+      `email`      = map.get("email").get.asInstanceOf[String],
+      `phone`      = map.get("phone").get.asInstanceOf[String],
+      `address`    = map.get("address").get.asInstanceOf[String],
+      `about`      = map.get("about").get.asInstanceOf[String],
       `registered` = map.get("registered").get.asInstanceOf[String],
-      `latitude` = map.get("latitude").get.asInstanceOf[Double],
-      `longitude` = map.get("longitude").get.asInstanceOf[Double],
-      `tags` = map.get("tags").get.asInstanceOf[List[String]],
+      `latitude`   = map.get("latitude").get.asInstanceOf[Double],
+      `longitude`  = map.get("longitude").get.asInstanceOf[Double],
+      `tags`       = map.get("tags").get.asInstanceOf[List[String]],
       `friends` = map
         .get("friends")
         .get
         .asInstanceOf[List[Map[String, Any]]]
         .map(readFriend),
-      `greeting` = map.get("greeting").get.asInstanceOf[String],
+      `greeting`      = map.get("greeting").get.asInstanceOf[String],
       `favoriteFruit` = map.get("favoriteFruit").get.asInstanceOf[String]
     )
 
   def readFriend(map: Map[String, Any]): Friend =
     Friend(
-      id = map.get("id").get.asInstanceOf[Int],
+      id   = map.get("id").get.asInstanceOf[Int],
       name = map.get("name").get.asInstanceOf[String]
     )
 }
